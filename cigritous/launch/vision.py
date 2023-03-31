392a77f74e853ba756d54f7fa309fa5a3ac95caa
rizkymille <muhammad.rizky912@ui.ac.id>
2023-03-31 23:54:42 +0700

    Add micrortps

--- cigritous/launch/vision.py
+++ cigritous/launch/vision.py
@@ -1,11 +1,24 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+from launch.actions import ExecuteProcess, TimerAction
+
 def generate_launch_description():
     
+    call_micrortps_agent = ExecuteProcess(
+        cmd=['micrortps_agent', ' -d', ' /dev/ttymxc2', '-b', '921600'], 
+        shell=True, 
+        output='screen')
+
     node = Node(
         package='cigritous',
         executable='program_vision.py',
         name='program_vision')
+    
+    delay_node = TimerAction(
+        actions=[node], 
+        period=3.0)
 
-    return LaunchDescription([node])
+    return LaunchDescription([
+        delay_node, 
+        call_micrortps_agent])
